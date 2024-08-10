bcef55b38db69d19a92b79a6806c5c78a75630d6
Vishu@1011 <vidhiok@gmail.com>
2024-08-10 07:53:51 -0700

    this keyword

---
+++ src/MobileMain.java
@@ -0,0 +1,16 @@
+import Aug.Mobile;
+
+public class MobileMain {
+    public static void main(String[] args) {
+        Mobile mobile = new Mobile("Iphone 16", 256, "Grey");
+        mobile.display();
+
+        Mobile m=new Mobile("iphone 14 pro");
+        m.display();
+
+        Mobile m1=new Mobile("samsung galaxy" , 200);
+        m1.display();
+
+
+    }
+}
